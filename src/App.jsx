22aa1b5cab57b97c22fff5ca002db8c78f69ac75
iyappanvcs-gb <iyappanrajagopal85@gmail.com>
2026-01-09 01:30:51 +0530

    Updated app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 function App()
   {
-    return  
+    return  (
     <>
       <center>
         <h1>App</h1> <br/> 
         <h2>This is an example for defining muultiple tag.</h2>
         <h2>This is an example of using the frament tag</h2>
       </center>
-      </>
+      </>);
   }
 
 function Helloworld()
