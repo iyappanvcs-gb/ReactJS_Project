99de489184219cb439a3b66eef95f6d00e2653c3
iyappanvcs-gb <iyappanrajagopal85@gmail.com>
2026-01-09 01:20:41 +0530

    ReactJS project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App()
+  {
+    return  
+    <>
+      <center>
+        <h1>App</h1> <br/> 
+        <h2>This is an example for defining muultiple tag.</h2>
+        <h2>This is an example of using the frament tag</h2>
+      </center>
+      </>
+  }
+
+function Helloworld()
+  {
+    return <h1>Helloworld</h1>
+  }
+
+  export {Helloworld,App}
